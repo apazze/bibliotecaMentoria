6700d62c1c810e332a4760b23841fe1cef58a18e
Alisson Pazze <alisson_pazze@terceiros.sicredi.com.br>
2022-06-02 10:38:47 -0300

    Refatoração ID das entidades.

--- src/main/java/com/alisson/biblioteca/entity/Autor.java
+++ src/main/java/com/alisson/biblioteca/entity/Autor.java
@@ -5,7 +5,7 @@ import java.util.List;
 @Entity
 public class Autor {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     @OneToMany(cascade = CascadeType.PERSIST)
--- src/main/java/com/alisson/biblioteca/entity/Livro.java
+++ src/main/java/com/alisson/biblioteca/entity/Livro.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Entity
 public class Livro {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String isbn;
     private String titulo;
@@ -31,4 +31,9 @@ public class Livro {
     public String getTitulo() { return titulo; }
 
     public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    @Override
+    public String toString() {
+        return "Id: ["+ this.id+"] Isbn: ["+this.isbn+"] Titulo: ["+this.titulo+"] Autor: ["+this.autor.getNome()+"]";
+    }
 }
